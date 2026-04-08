245c611a17673568c4fc07707df7e0738f697e0a
sandhyareddy451 <vemula.sandhya51@gmail.com>
2026-04-08 10:24:35 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Create directory
+
+mkdir -p src
+mkdir -p research
+
+# Create directofiles
+ touch src/__init__.py
+ touch src/helper.py
+ touch src/prompt.py
+ touch .env
+ touch setup.py
+ touch app.py
+ touch research/trails.ipynb
+ touch requirements.txt
+
+echo "Directory and files created sucessfully!."
